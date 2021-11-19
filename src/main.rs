4d1379ebaefe34e181647248c78570cd7d384021
Rance <rance@outlawcoding.com>
2021-11-19 05:58:26 -0700

    add turn state

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod map;
 mod map_builder;
 mod spawner;
 mod systems;
+mod turn_state;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -22,6 +23,7 @@ mod prelude {
     pub use crate::map_builder::*;
     pub use crate::spawner::*;
     pub use crate::systems::*;
+    pub use crate::turn_state::*;
 }
 
 use prelude::*;
@@ -47,6 +49,7 @@ impl State {
             .for_each(|pos| spawn_enemy(&mut ecs, &mut rng, pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
+        resources.insert(TurnState::AwaitingInput);
         Self {
             ecs,
             resources,
